080909217da7733b9f8c5e98448e85b89c7947ba
Yilchik <yulyayulia2@gmail.com>
2024-03-30 13:17:27 +0100

    2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,36 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const delay = formData.get('delay');
+  const state = formData.get('state');
+
+  const promise = new Promise((resolve, reject) => {
+    if (state === 'fulfilled') {
+      setTimeout(() => resolve(delay), delay);
+    } else if (state === 'rejected') {
+      setTimeout(() => reject(delay), delay);
+    }
+  });
+
+  promise
+    .then(success =>
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      })
+    )
+    .catch(error =>
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      })
+    );
+});
